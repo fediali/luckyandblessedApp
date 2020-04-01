d352e6e87a1e428e25bf62bb7491828b8291a124
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2020-04-01 22:46:52 +0500

    First Commit

---
+++ components/SignIn.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import {View,Text} from "react-native"
+class SignIn extends Component{
+    render(){
+        return(
+
+            <View>
+                <Text>HEllo World</Text>
+            </View>
+        )
+    }
+
+}
+
+export default SignIn
