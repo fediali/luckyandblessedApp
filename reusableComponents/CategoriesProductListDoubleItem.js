2c54044e3480f1a6ec584d42c178f594ceea3753
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2020-04-07 21:28:33 +0500

    fix ui bug in categoriesproduct

--- reusableComponents/CategoriesProductListDoubleItem.js
+++ reusableComponents/CategoriesProductListDoubleItem.js
@@ -21,7 +21,7 @@ class CategoriesProductListDoubleItem extends PureComponent {
             <View style={{ maxWidth: Width / 2}}>
                 <Image source={this.props.imageUrl} style={{ height: Height * 0.3, width: Width * 0.43,justifyContent:"center",borderRadius:6}} />
                 {/* orignal width is 0.12 currently 0.30 due to long height image, height is 0.26 currently 0.20 */}
-                <Text style={{ maxWidth:Width * 0.43,fontFamily: "Montserrat-Medium", fontSize: 16, lineHeight: 20, color: "#2d2d2f",textAlign:"justify"}}>{this.props.name1}</Text>
+                <Text style={{ maxWidth:Width * 0.43,fontFamily: "Montserrat-Medium", fontSize: 16, lineHeight: 20, color: "#2d2d2f"}}>{this.props.name1}</Text>
                 <Text style={{ marginTop:7,fontFamily: "Avenir-Book", fontSize: 14, lineHeight: 18, color: "#2967ff", maxWidth:Width * 0.5}}>{this.props.name2}</Text>
                 <Text style={{ fontSize: 16, lineHeight: 20, fontFamily: "Montserrat-Medium", color: "#2d2d2f" }}>{this.props.price1}</Text>
             </View>
