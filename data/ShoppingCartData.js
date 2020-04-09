c96fb06b972649cee901dbc042f7d23294fec648
iasim90 <iasim90@gmail.com>
2020-04-09 23:42:08 +0500

    swipe right to delete on shopping cart

---
+++ data/ShoppingCartData.js
@@ -0,0 +1,41 @@
+var itemList = [
+    {
+        itemNum: 0,
+        name: '3-Stripes Shirt',
+        price: 48.99,
+        unitPrice: 19.40,
+        sizes: '2(S), 2(M), 2(L)',
+        selectedColor: 'Turquoise',
+        availableColors: ['#eb4034', '#05c2bd', '#f4f719', '#0caac9', '#e629df'],
+        quantity: 14,
+        unknownNum: 6,
+        hexColor: '#05c2bd',
+        path: "../static/item_cart1.png"
+    }, {
+        itemNum: 1,
+        name: 'Tuxedo Blouse',
+        price: 34.90,
+        unitPrice: 50.00,
+        sizes: '2(S), 2(M), 2(L)',
+        selectedColor: 'Turquoise',
+        availableColors: ['#eb4034', '#05c2bd', '#f4f719', '#0caac9', '#e629df'],
+        quantity: 14,
+        unknownNum: 12,
+        hexColor: '#3c3c3e',
+        path: "../static/item_cart2.png"
+    }, {
+        itemNum: 2,
+        name: 'Linear Near Tee',
+        price: 19.99,
+        unitPrice: 29.50,
+        sizes: '2(S), 2(M), 2(L)',
+        selectedColor: 'Turquoise',
+        availableColors: ['#eb4034', '#05c2bd', '#f4f719', '#0caac9', '#e629df'],
+        quantity: 14,
+        unknownNum: 12,
+        hexColor: '#ff671c',
+        path: "../static/item_cart3.png"
+    }
+];
+
+module.exports = itemList;
