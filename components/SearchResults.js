8576266f0b7dca642533891337fa657c1cb3fd84
zayantharani <zayantharani@gmail.com>
2020-04-09 16:24:41 +0500

    SearchResults done

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -14,6 +14,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {Icon} from 'react-native-elements';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
+//TODO: Check for the last row
 export default class SearchResults extends Component {
   constructor(props) {
     super(props);
