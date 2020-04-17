53b82273c6facd3a178e1a9f0cd57f1b23a159ee
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2020-04-17 20:09:12 +0500

    put data delete data and post data files completed

---
+++ reusableComponents/API/PostData.js
@@ -0,0 +1,22 @@
+export default PostData = (url,jsonObj) => {
+    let h = new Headers();
+    h.append(
+        'Authorization',
+        'Basic: emF5YW50aGFyYW5pQGdtYWlsLmNvbTo3bjE3N0JFRTc5OXYyazRIeThkNVdKNDBIOXoxdzBvMw==',
+    );
+    h.append('Accept', 'application/json');
+    // const formData = new FormData();
+    // formData.append('first_name', profile.firstName);
+    // formData.append('last_name', profile.lastName);
+    // formData.append('email', profile.email);
+
+    let req = new Request(url, {
+        headers: h,
+        method: 'POST',
+        body: JSON.stringify(jsonObj)
+
+    });
+
+    return fetch(req)
+
+}
