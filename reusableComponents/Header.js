ea88ab42549f46acbd2d3fc800a74a00efb01555
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2020-04-18 22:26:56 +0500

    ..

--- reusableComponents/Header.js
+++ reusableComponents/Header.js
@@ -132,7 +132,7 @@ class Header extends PureComponent {
                     }
 
                     {this.props.homePage ?
-                        <View >
+                        <View>
                             <Text style={{ fontFamily: "Montserrat-SemiBold", fontSize: 18, lineHeight: 22 }}>{this.props.centerText}</Text>
                             <Text style={styles.homePersonText}>{this.props.person}</Text>
                         </View>
