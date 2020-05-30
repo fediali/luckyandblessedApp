34efe4df0fb9e319bc35ec9062a3c524a43397d9
iasim90 <iasim90@gmail.com>
2020-05-31 00:50:55 +0500

    Delivery Screen API integration and validation of inputs

--- components/Delivery.js
+++ components/Delivery.js
@@ -11,28 +11,190 @@ import {
     InteractionManager
 } from 'react-native'
 
+import { Icon } from 'react-native-elements'
 import styles from './Styles/Style'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../reusableComponents/Header'
 import Footer from '../reusableComponents/Footer'
 import { TextInput } from 'react-native-gesture-handler';
 import Shimmer from 'react-native-shimmer';
+import Globals from '../Globals';
+import RetrieveDataAsync from '../reusableComponents/AsyncStorage/RetrieveDataAsync';
+
+
+const STORAGE_USER = Globals.STORAGE_USER;
+const baseUrl = Globals.baseUrl;
+
 
 class Delivery extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            isReady: false
+            isReady: false,
+            fullName: "",
+            fullNameError: "",
+            streetAddress: "",
+            streetAddressError: "",
+            cityTown: "",
+            cityTownError: "",
+            stateText: "",
+            stateTextError: "",
+            zipCode: "",
+            zipCodeError: "",
+            email: "",
+            emailError: "",
+            phoneNumber: "",
+            phoneNumberError: "",
         }
     }
 
 
+
     componentDidMount() {
         InteractionManager.runAfterInteractions(() => {
-            this.setState({ isReady: true })
+            this.setState({ isReady: false })
+            // Retriving the user_id
+            RetrieveDataAsync(STORAGE_USER).then((user) => {
+                this.getData(JSON.parse(user));
+            });
         })
     };
+
+    getData = (user) => {
+        var promises = [];
+        promises.push(
+            GetData(
+                baseUrl +
+                `api/userprofiles?user_id=${user.user_id}`,
+            ),
+
+        );
+        console.log(baseUrl +
+            `api/userprofiles?user_id=${user.user_id}`);
+        Promise.all(promises)
+            .then((promiseResponses) => {
+                Promise.all(promiseResponses.map((res) => res.json()))
+                    .then((responses) => {
+                        if("firstname" in responses[0].users[0] && "lastname" in responses[0].users[0])
+                        this.setState({
+                            fullName: responses[0].users[0].firstname+" "+responses[0].users[0].lastname,
+                        })
+                        
+                        if("email" in responses[0].users[0])
+                        this.setState({
+                            email: responses[0].users[0].email,
+                        })
+                        
+                        if("street_address" in responses[0].users[0])
+                        this.setState({
+                            streetAddress: responses[0].users[0].street_address,
+                        })
+                        
+                        if("zip" in responses[0].users[0])
+                        this.setState({
+                            zipCode: responses[0].users[0].zip,
+                        })
+                        
+                        if("state" in responses[0].users[0])
+                        this.setState({
+                            stateText: responses[0].users[0].state,
+                        })
+                        
+                        if("city" in responses[0].users[0])
+                        this.setState({
+                            cityTown: responses[0].users[0].city,
+                        })
+                        
+                        if("phone_number" in responses[0].users[0])
+                        this.setState({
+                            phoneNumber: responses[0].users[0].phone_number,
+                        })
+
+                        this.setState({isReady: true})
+                    })
+                    .catch((ex) => {
+                        console.log('Inner Promise', ex);
+                    });
+            })
+            .catch((ex) => {
+                console.log('Outer Promise', ex);
+                alert(ex);
+            });
+    };
+
+    validateAndRedirect=()=>{
+        if(this.isValid()){
+            //TODO: Call post userdetails API here. All input data is stored in state
+            this.props.navigation.navigate("Payment")
+        } 
+    }
+
+    isValid() {
+        let validFlag = true
+
+        if (this.state.fullName == "") {
+            this.setState({ fullNameError: "Full Name is required." })
+            validFlag = false;
+        } else {
+            this.setState({ fullNameError: "" })
+        }
+
+        if (this.state.streetAddress == "") {
+            this.setState({ streetAddressError: "Street address is required." })
+            validFlag = false;
+        } else {
+            this.setState({ streetAddressError: "" })
+        }
+
+        if (this.state.cityTown == "") {
+            this.setState({ cityTownError: "City/Town is required." })
+            validFlag = false;
+        } else {
+            this.setState({ cityTownError: "" })
+        }
+
+        if (this.state.stateText == "") {
+            this.setState({ stateTextError: "State is required." })
+            validFlag = false;
+        } else {
+            this.setState({ stateTextError: "" })
+        }
+
+        if (this.state.zipCode == "") {
+            this.setState({ zipCodeError: "Zip code is required." })
+            validFlag = false;
+        } else {
+            this.setState({ zipCodeError: "" })
+        }
+
+        if (this.state.email == "") {
+            this.setState({ emailError: "Email is required." })
+            validFlag = false;
+        } else {
+            this.setState({ emailError: "" })
+        }
+
+        if (this.state.phoneNumber == "") {
+            this.setState({ phoneNumberError: "Phone number is required." })
+            validFlag = false;
+        } else {
+            this.setState({ phoneNumberError: "" })
+        }
+
+        return validFlag;
+    }
+
+    showErrorMessage(errorMessage) {
+        return (
+            <View style={innerStyles.errorTextMainView}>
+                <Icon size={30} name='md-information-circle-outline' type='ionicon' color='#FF0000' />
+                <Text style={innerStyles.errorTextText}>{errorMessage}</Text>
+            </View>
+        )
+    }
+
+    
 
     render() {
         if (!this.state.isReady) {
@@ -78,29 +240,37 @@ class Delivery extends Component {
                             </View>
 
                             <View style={styles.inputView}>
-                                <TextInput style={styles.input} placeholder="Full name" />
+                                <TextInput style={styles.input} placeholder="Full name" value={this.state.fullName} onChangeText={(text) => { this.setState({ fullName: text }) }} />
                             </View>
+                            {this.state.fullNameError != "" ? this.showErrorMessage(this.state.fullNameError) : <View></View>}
+
 
                             <View style={styles.inputView}>
-                                <TextInput style={styles.input} placeholder="Street address" />
+                                <TextInput style={styles.input} placeholder="Street address" value={this.state.streetAddress}  onChangeText={(text) => { this.setState({ streetAddress: text }) }} />
                             </View>
+                            {this.state.streetAddressError != "" ? this.showErrorMessage(this.state.streetAddressError) : <View></View>}
 
                             <View style={styles.inputView}>
-                                <TextInput style={styles.input} placeholder="City / Town" />
+                                <TextInput style={styles.input} placeholder="City / Town" value={this.state.cityTown}  onChangeText={(text) => { this.setState({ cityTown: text }) }} />
                             </View>
+                            {this.state.cityTownError != "" ? this.showErrorMessage(this.state.cityTownError) : <View></View>}
 
                             <View style={styles.inputView}>
-                                <TextInput style={styles.input} placeholder="State" />
-                                <TextInput style={[styles.input, innerStyles.marginStart]} placeholder="Zip code" />
+                                <TextInput style={styles.input} placeholder="State" value={this.state.stateText}  onChangeText={(text) => { this.setState({ stateText: text }) }} />
+                                <TextInput style={[styles.input, innerStyles.marginStart]} keyboardType={'number-pad'} placeholder="Zip code" />
                             </View>
+                            {this.state.stateTextError != "" ? this.showErrorMessage(this.state.stateTextError) : <View></View>}
+                            {this.state.zipCodeError != "" ? this.showErrorMessage(this.state.zipCodeError) : <View></View>}
+                            <View style={styles.inputView}>
+                                <TextInput style={styles.input} placeholder="Phone number" keyboardType={'number-pad'} value={this.state.phoneNumber}  onChangeText={(text) => { this.setState({ phoneNumber: text }) }} />
+                            </View>
+                            {this.state.phoneNumberError != "" ? this.showErrorMessage(this.state.phoneNumberError) : <View></View>}
 
                             <View style={styles.inputView}>
-                                <TextInput style={styles.input} placeholder="Phone number" />
+                                <TextInput style={styles.input} placeholder="Email address" value={this.state.email}  onChangeText={(text) => { this.setState({ email: text }) }} />
                             </View>
+                            {this.state.emailError != "" ? this.showErrorMessage(this.state.emailError) : <View></View>}
 
-                            <View style={styles.inputView}>
-                                <TextInput style={styles.input} placeholder="Email address" />
-                            </View>
                         </View>
                         <View style={innerStyles.showOrderView}>
                             <View style={innerStyles.orderViewNested}>
@@ -113,7 +283,7 @@ class Delivery extends Component {
                             </View>
                         </View>
                         <View style={[styles.buttonContainer, innerStyles.buttonStyles]}>
-                            <TouchableOpacity activeOpacity={0.5} style={[innerStyles.buttonPaymentMethod]} onPress={() => { this.props.navigation.navigate("Payment") }}>
+                            <TouchableOpacity activeOpacity={0.5} style={[innerStyles.buttonPaymentMethod]} onPress={()=>this.validateAndRedirect()}>
                                 <Text
                                     style={[
                                         styles.buttonText,
@@ -135,6 +305,9 @@ let width = Dimensions.get('window').width;
 let height = Dimensions.get('window').height;
 
 const innerStyles = StyleSheet.create({
+    errorTextMainView: { width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', paddingHorizontal: 15 },
+    errorTextText: { paddingHorizontal: 10, color: '#FF0000', maxWidth: '93%' },
+
     paymentText: {
         color: '#ffffff',
         fontSize: 18,
@@ -235,7 +408,7 @@ const innerStyles = StyleSheet.create({
     loaderImage: { height: 200, width: 200 },
     textAlignLeft: { textAlign: "left" },
     textAlignCenter: { textAlign: 'center' },
-    alignCenter:{
+    alignCenter: {
         alignItems: 'center',
     }
 })
