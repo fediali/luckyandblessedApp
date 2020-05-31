f85cdbea5c56f71b8783762dbfd7d31e843148b2
zayantharani <zayantharani@gmail.com>
2020-05-31 16:18:56 +0500

    Send tax id file

--- reusableComponents/OrderProductListItem.js
+++ reusableComponents/OrderProductListItem.js
@@ -1,41 +1,50 @@
-import React, { PureComponent } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import FastImage from 'react-native-fast-image'
+import React, {PureComponent} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import FastImage from 'react-native-fast-image';
 
 export default class OrderProductListItem extends PureComponent {
   render() {
-    console.log(this.props.data)
+    console.log(this.props.data);
     return (
-      <View
-        style={styles.topLevelView}>
-        <View style={styles.fdRow}>
-          <View style={styles.thumbnailView}>
-          <FastImage
-            style={[styles.thumbnailImage]}
-            resizeMode="contain"
-            source={{uri: this.props.data.imageUrl}}
-          />
-          </View>
-         
-          <View style={[styles.priceTextView]}>
-            <View style={[styles.fdRow]}>
-              <Text style={[styles.itemNameText]}>{this.props.data.itemName}</Text>
-              <Text style={[styles.itemNameText]}>${this.props.data.totalPrice}</Text>
+      <View style={styles.padBottom20}>
+        <View style={styles.topLevelView}>
+          <View style={styles.fdRow}>
+            <View style={styles.thumbnailView}>
+              <FastImage
+                style={[styles.thumbnailImage]}
+                resizeMode="contain"
+                source={{uri: this.props.data.imageUrl}}
+              />
             </View>
-            <View style={[styles.fdRow]}>
-              <Text style={[styles.itemUnitPriceText]}>Unit price</Text>
-              <Text style={[styles.lightText, styles.mart4]}>${this.props.data.unitPrice}</Text>
-            </View>
-            <Text style={styles.lightText}>SIZE: {this.props.data.size}</Text>
-            <Text style={styles.lightText}>Color: {this.props.data.color}</Text>
-            <Text style={[styles.lightText, styles.marb20]}>Quantity: {this.props.data.quantity}</Text>
-          </View>
-        </View>
-        <View>
 
+            <View style={[styles.priceTextView]}>
+              <View style={[styles.fdRow]}>
+                <Text style={[styles.itemNameText]}>
+                  {this.props.data.itemName}
+                </Text>
+                <Text style={[styles.itemNameText]}>
+                  ${this.props.data.totalPrice}
+                </Text>
+              </View>
+              <View style={[styles.fdRow]}>
+                <Text style={[styles.itemUnitPriceText]}>Unit price</Text>
+                <Text style={[styles.lightText, styles.mart4]}>
+                  ${this.props.data.unitPrice}
+                </Text>
+              </View>
+              <Text style={styles.lightText}>SIZE: {this.props.data.size}</Text>
+              <Text style={styles.lightText}>
+                Color: {this.props.data.color}
+              </Text>
+              <Text style={[styles.lightText, styles.marb20]}>
+                Quantity: {this.props.data.quantity}
+              </Text>
+            </View>
+          </View>
+          <View></View>
         </View>
       </View>
-    )
+    );
   }
 }
 
@@ -47,23 +56,24 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     letterSpacing: 0,
     color: '#2d2d2f',
-    maxWidth: '60%'
+    maxWidth: '60%',
   },
 
   lightText: {
-    fontFamily: "Avenir-Book",
+    fontFamily: 'Avenir-Book',
     fontSize: 14,
-    fontWeight: "normal",
-    fontStyle: "normal",
+    fontWeight: 'normal',
+    fontStyle: 'normal',
     lineHeight: 18,
     letterSpacing: 1,
     color: '#8d8d8e',
   },
+  padBottom20: {paddingBottom: 20},
   leftAligned: {
-    textAlign: 'left'
+    textAlign: 'left',
   },
   rightAligned: {
-    textAlign: 'right'
+    textAlign: 'right',
   },
   itemUnitPriceText: {
     fontFamily: 'Avenir-Book',
@@ -72,22 +82,25 @@ const styles = StyleSheet.create({
     lineHeight: 18,
     letterSpacing: 0,
     color: '#2967ff',
-    marginVertical: 4
+    marginVertical: 4,
   },
   thumbnailImage: {
     height: 100,
     width: 100,
   },
   thumbnailView: {
-    backgroundColor: "#f6f6f6",
+    backgroundColor: '#f6f6f6',
     borderRadius: 6,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
-  topLevelView: { flexDirection: 'row', justifyContent: 'space-between',alignItems: 'center', marginHorizontal: 20},
-  fdRow: { flexDirection: 'row', justifyContent: 'space-between' },
-  priceTextView: { flexDirection: 'column' , paddingHorizontal:10},
-  marb20: { marginBottom: 20 },
-  mart4: { marginTop: 4 },
-
-
-})
+  topLevelView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  fdRow: {flexDirection: 'row', justifyContent: 'space-between'},
+  priceTextView: {flexDirection: 'column', paddingHorizontal: 10},
+  marb20: {marginBottom: 20},
+  mart4: {marginTop: 4},
+});
--- reusableComponents/OrdersProduct.js
+++ reusableComponents/OrdersProduct.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import {
   Text,
   View,
@@ -8,9 +8,9 @@ import {
   Dimensions,
   FlatList,
   TouchableOpacity,
-  Linking
+  Linking,
 } from 'react-native';
-import { Icon } from 'react-native-elements';
+import {Icon} from 'react-native-elements';
 import OrderProductListItem from './OrderProductListItem';
 const Globals = require('../Globals');
 
@@ -30,31 +30,28 @@ export default class ordersProduct extends PureComponent {
       isLoadingMoreListData: false,
       showZeroProductScreen: false,
       trackingNumber: null,
-      trackingURL: null
+      trackingURL: null,
     };
   }
 
   componentDidMount() {
     InteractionManager.runAfterInteractions(() => {
-      this.setState({ isReady: false });
+      this.setState({isReady: false});
       this.getData(this.props.orderId);
     });
   }
 
   getTrackingData = (shipmentID, index) => {
     var promises = [];
-    promises.push(
-      GetData(
-        baseUrl +
-        `api/shipments/${shipmentID}`,
-      ),
-    );
+    promises.push(GetData(baseUrl + `api/shipments/${shipmentID}`));
     Promise.all(promises)
       .then((promiseResponses) => {
         Promise.all(promiseResponses.map((res) => res.json()))
           .then((responses) => {
-           
-            this.setState({trackingNumber: responses[0].tracking_number, trackingURL: responses[0].carrier_info.tracking_url});
+            this.setState({
+              trackingNumber: responses[0].tracking_number,
+              trackingURL: responses[0].carrier_info.tracking_url,
+            });
           })
           .catch((ex) => {
             console.log('Inner Promise', ex);
@@ -64,54 +61,68 @@ export default class ordersProduct extends PureComponent {
         console.log('Outer Promise', ex);
         alert(ex);
       });
-  }
+  };
 
   getData = (orderId) => {
     var promises = [];
-    promises.push(
-      GetData(
-        baseUrl +
-        `api/orders/${orderId}`,
-      ),
-    );
+    promises.push(GetData(baseUrl + `api/orders/${orderId}`));
 
     Promise.all(promises)
       .then((promiseResponses) => {
         Promise.all(promiseResponses.map((res) => res.json()))
           .then((responses) => {
-
-            productGroupKeys = Object.keys(responses[0].product_groups[0].products)
-            let productArray = []
-            let jsonProducts = {}
+            productGroupKeys = Object.keys(
+              responses[0].product_groups[0].products,
+            );
+            let productArray = [];
+            let jsonProducts = {};
             //TODO: No Size attribute in response.
             //TODO: No Color attribute in response.
             for (var i = 0; i < productGroupKeys.length; i++) {
-              jsonProducts['itemNum'] = productGroupKeys[i].toString()
-              jsonProducts['totalPrice'] = responses[0].product_groups[0].products[productGroupKeys[i]].price * responses[0].product_groups[0].products[productGroupKeys[i]].amount
-              jsonProducts['itemName'] = responses[0].product_groups[0].products[productGroupKeys[i]].product
-              jsonProducts['size'] = "Not available"
-              jsonProducts['color'] = "Not available"
-              jsonProducts['quantity'] = responses[0].product_groups[0].products[productGroupKeys[i]].amount
-              jsonProducts['unitPrice'] = responses[0].product_groups[0].products[productGroupKeys[i]].price
-              if ('detailed' in responses[0].product_groups[0].products[productGroupKeys[i]].main_pair) {
-                jsonProducts['imageUrl'] = responses[0].product_groups[0].products[productGroupKeys[i]].main_pair.detailed.image_path
+              jsonProducts['itemNum'] = productGroupKeys[i].toString();
+              jsonProducts['totalPrice'] =
+                responses[0].product_groups[0].products[productGroupKeys[i]]
+                  .price *
+                responses[0].product_groups[0].products[productGroupKeys[i]]
+                  .amount;
+              jsonProducts['itemName'] =
+                responses[0].product_groups[0].products[
+                  productGroupKeys[i]
+                ].product;
+              jsonProducts['size'] = 'Not available';
+              jsonProducts['color'] = 'Not available';
+              jsonProducts['quantity'] =
+                responses[0].product_groups[0].products[
+                  productGroupKeys[i]
+                ].amount;
+              jsonProducts['unitPrice'] =
+                responses[0].product_groups[0].products[
+                  productGroupKeys[i]
+                ].price;
+              if (
+                'detailed' in
+                responses[0].product_groups[0].products[productGroupKeys[i]]
+                  .main_pair
+              ) {
+                jsonProducts['imageUrl'] =
+                  responses[0].product_groups[0].products[
+                    productGroupKeys[i]
+                  ].main_pair.detailed.image_path;
               } else {
-                jsonProducts['imageUrl'] = 'https://picsum.photos/200'
+                jsonProducts['imageUrl'] = 'https://picsum.photos/200';
               }
               if (responses[0].shipment_ids.length > 0) {
-                jsonProducts['shipment_id'] = responses[0].shipment_ids
-                this.setState({ isTrackingReady: false })
-                this.getTrackingData(jsonProducts['shipment_id'], i)
+                jsonProducts['shipment_id'] = responses[0].shipment_ids;
+                this.setState({isTrackingReady: false});
+                this.getTrackingData(jsonProducts['shipment_id'], i);
               } else {
-                this.setState({ isTrackingReady: true })
+                this.setState({isTrackingReady: true});
               }
-              productArray[i] = jsonProducts
-              jsonProducts = {}
-
-
+              productArray[i] = jsonProducts;
+              jsonProducts = {};
             }
 
-            this.setState({ orders: productArray, isReady: true })
+            this.setState({orders: productArray, isReady: true});
           })
           .catch((ex) => {
             console.log('Inner Promise', ex);
@@ -129,10 +140,13 @@ export default class ordersProduct extends PureComponent {
 
   renderFlatListFooter = (item) => {
     var listFooter = (
-      <TouchableOpacity style={styles.trackingStyle}
-      onPress={ ()=>{ Linking.openURL(this.state.trackingURL)}}>
+      <TouchableOpacity
+        style={styles.trackingStyle}
+        onPress={() => {
+          Linking.openURL(this.state.trackingURL);
+        }}>
         <Text style={styles.orderIdText}>
-        Track: {this.state.trackingNumber}
+          Track: {this.state.trackingNumber}
         </Text>
         <View style={styles.mar19}>
           <Icon size={20} name="right" type="antdesign" />
@@ -140,21 +154,21 @@ export default class ordersProduct extends PureComponent {
       </TouchableOpacity>
     );
     return listFooter;
-  }
+  };
   render() {
     let Height = Dimensions.get('window').height;
     let Width = Dimensions.get('window').width;
     if (!this.state.isReady) {
       return (
-        <View style={styles.loader}><ActivityIndicator size="large" /></View>
-      )
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
     }
     return (
       <View>
-        {this.state.orders.map((item,index)=>{
-          return(
-            <OrderProductListItem key={index.toString()} data={item}/>
-          )
+        {this.state.orders.map((item, index) => {
+          return <OrderProductListItem key={index.toString()} data={item} />;
         })}
         {/* <FlatList
           style={styles.flatlistStyle}
@@ -167,7 +181,18 @@ export default class ordersProduct extends PureComponent {
 
         /> */}
         <View style={styles.dividerLine}></View>
-
+        <TouchableOpacity
+          style={styles.trackingStyle}
+          onPress={() => {
+            Linking.openURL(this.state.trackingURL);
+          }}>
+          <Text style={styles.orderIdText}>
+            Track: {this.state.trackingNumber}
+          </Text>
+          <View style={styles.mar19}>
+            <Icon size={20} name="right" type="antdesign" />
+          </View>
+        </TouchableOpacity>
 
         <View style={styles.dividerLine}></View>
 
@@ -185,7 +210,12 @@ const styles = StyleSheet.create({
     width: 100,
     borderRadius: 6,
   },
-  loader: { flex: 1, alignItems: "center", justifyContent: "center" },
+  loader: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+  },
   lightText: {
     fontFamily: 'Avenir-Book',
     fontSize: 14,
@@ -207,13 +237,13 @@ const styles = StyleSheet.create({
     color: '#2d2d2f',
     marginVertical: 19,
   },
-  seperator: { paddingBottom: 20 },
-  flatlistStyle: { marginTop: 13 },
-  dividerLine: { backgroundColor: '#f6f6f6', paddingTop: 1 },
+  seperator: {paddingBottom: 20},
+  flatlistStyle: {marginTop: 13},
+  dividerLine: {backgroundColor: '#f6f6f6', paddingTop: 1},
   trackingStyle: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginHorizontal: 20,
   },
-  mar19: { marginTop: 19 },
+  mar19: {marginTop: 19},
 });
