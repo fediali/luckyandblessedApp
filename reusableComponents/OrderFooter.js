c3a333af677414888b905de7139c51a72c6deadf
iasim90 <iasim90@gmail.com>
2020-06-04 00:53:57 +0500

    Shopping cart API integrated

--- reusableComponents/OrderFooter.js
+++ reusableComponents/OrderFooter.js
@@ -1,5 +1,5 @@
-import React, {PureComponent} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, { PureComponent } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import styles from '../components/Styles/Style';
 
 export default class OrderFooter extends PureComponent {
@@ -13,7 +13,7 @@ export default class OrderFooter extends PureComponent {
               Order amount:{' '}
             </Text>
             <Text style={[styles.buttonText, innerStyles.orderAmountValueText]}>
-              $103.88
+              ${this.props.totalCost}
             </Text>
           </View>
           <View style={innerStyles.orderRowView}>
@@ -22,7 +22,7 @@ export default class OrderFooter extends PureComponent {
             </Text>
             <Text
               style={[innerStyles.lightText, innerStyles.orderAmountValueText]}>
-              -$50.00
+              ${this.props.discount}
             </Text>
           </View>
           <View style={innerStyles.orderRowView}>
@@ -30,7 +30,7 @@ export default class OrderFooter extends PureComponent {
               Shipping:{' '}
             </Text>
             <Text style={[innerStyles.shippingText]}>
-              Shipping will be added later
+              {this.props.shipAddress}
             </Text>
           </View>
           <View style={innerStyles.orderRowView}>
@@ -38,64 +38,64 @@ export default class OrderFooter extends PureComponent {
               Order Total:{' '}
             </Text>
             <Text style={[styles.buttonText, innerStyles.orderAmountValueText]}>
-              $53.88
+              ${this.props.totalCost - this.props.discount}
             </Text>
           </View>
         </View>
-        
+
       </View>
     );
   }
 }
 
 const innerStyles = StyleSheet.create({
-    showOrderView: {
-        paddingTop: 15,
-        flexDirection: 'column',
-        flex: 1,
-        width: '100%',
-        backgroundColor: '#f6f6f6'
-    },
-    orderRowView: {
-        flexDirection: 'row', paddingHorizontal: 20, justifyContent: "space-between"
-    },
-    orderAmountText: {
-        fontSize: 18, lineHeight: 30
-    },
-    orderAmountValueText: {
-        flex: 1, fontSize: 18, lineHeight: 30, textAlign: 'right'
-    },
-    shippingText: {fontFamily: "Avenir-Medium", fontSize: 16},
-    orderGiftText: {
-        lineHeight: 30
-    },
-    lightText: {
-        fontFamily: "Avenir-Book",
-        fontSize: 14,
-        fontWeight: "normal",
-        fontStyle: "normal",
-        lineHeight: 18,
-        letterSpacing: 1,
-        color: '#8d8d8e',
-    },
-    orderButtonView: {
-        paddingHorizontal: 30, width: '100%',
-        backgroundColor: '#f6f6f6',
-        paddingBottom: 20
-    },
-    orderButtonText: {
-        color: '#ffffff',
-        fontSize: 18,
-        lineHeight: 22
-    },
-    buttonPaymentMethod: {
-        width: '100%',
-        backgroundColor: '#2967ff',
-        borderRadius: 6,
-        paddingVertical: 15,
-        paddingHorizontal: 30,
-        marginTop: 15,
-    },
+  showOrderView: {
+    paddingTop: 15,
+    flexDirection: 'column',
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#f6f6f6'
+  },
+  orderRowView: {
+    flexDirection: 'row', paddingHorizontal: 20, justifyContent: "space-between"
+  },
+  orderAmountText: {
+    fontSize: 18, lineHeight: 30
+  },
+  orderAmountValueText: {
+    flex: 1, fontSize: 18, lineHeight: 30, textAlign: 'right'
+  },
+  shippingText: { fontFamily: "Avenir-Medium", fontSize: 16 },
+  orderGiftText: {
+    lineHeight: 30
+  },
+  lightText: {
+    fontFamily: "Avenir-Book",
+    fontSize: 14,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    lineHeight: 18,
+    letterSpacing: 1,
+    color: '#8d8d8e',
+  },
+  orderButtonView: {
+    paddingHorizontal: 30, width: '100%',
+    backgroundColor: '#f6f6f6',
+    paddingBottom: 20
+  },
+  orderButtonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    lineHeight: 22
+  },
+  buttonPaymentMethod: {
+    width: '100%',
+    backgroundColor: '#2967ff',
+    borderRadius: 6,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    marginTop: 15,
+  },
 
 })
 
